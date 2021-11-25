7ca5569b1430cbe62b90409ec4eb31b14b49b501
zx212 <sskhoda44@gmail.com>
2021-11-25 14:43:09 +0900

    プロジェクト ファイルを追加します。

---
+++ ConstructorSampe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace ConstructorSampe
+{
+    class ConstructorSampeMain
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(
+                 InteractiveInput.InputFloat("幅："),
+                 InteractiveInput.InputFloat("高さ："));
+            Console.WriteLine($"面積は{triangle.GetArea()}");
+            Triangle triangleedge = new Triangle(
+                 InteractiveInput.InputFloat("辺A："),
+                 InteractiveInput.InputFloat("辺B："),
+                 InteractiveInput.InputFloat("辺C："));
+            Console.WriteLine($"辺は" + Math.Sqrt(triangleedge.GetHeronsFormula()));
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSampe
+{
+    class Triangle
+    {
+        float Length;
+        float Height;
+        float SidesA;
+        float SidesB;
+        float SidesC;
+
+        public Triangle(float l, float h)
+        {
+            this.Length = l;
+            this.Height = h;
+        }
+        public Triangle(float a, float b, float c)
+        {
+            this.SidesA = a;
+            this.SidesB = b;
+            this.SidesC = c;
+        }
+
+        public float GetArea()
+        {
+            return (Length * Height / 2);
+        }
+
+        public float GetHeronsFormula()
+        {
+            float s = (SidesA + SidesB + SidesC) / 2;
+            return (s * (s - SidesA) * (s - SidesB) * (s - SidesC));
+        }
+    }
+}
